a03c41d8b5b3946eeddbf1898a9e628bf3d2c15c
jjjSilence <884604421@qq.com>
2018-04-23 17:57:27 +0800

    discovery

--- pages/discovery/discovery.js
+++ pages/discovery/discovery.js
@@ -1,13 +1,20 @@
 Page({
   data:{
-    navTab: ["推荐", "圆桌", "热门", "收藏"],
-    currentNavtab: "0",
-
+    navTab:['推荐','圆桌','热门','收藏'],
+    currentNavtab:'0',
+    imgUrls:[
+      '../../images/24213.jpg',
+      '../../images/24280.jpg',
+      '../../images/1444983318907-_DSC1826.jpg'
+    ],
+    indicatorDots:false,
+    autoplay:true,
+    interval:5000,
+    duration:1000
   },
-
-   switchTab: function (e) {
+  switchTab:function(e){
     this.setData({
-      currentNavtab: e.currentTarget.dataset.idx
-    });
+      currentNavtab:e.currentTarget.dataset.idx
+    })
   },
-});
+})
